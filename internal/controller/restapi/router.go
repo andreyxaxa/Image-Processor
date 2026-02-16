b6777efbbf4f164dd78aff51883d065f570eafec
Petrov Andrey <4ndrew1337@mail.ru>
2026-02-16 03:58:59 +0300

    feat(core): restapi router now accepts config for swagger

--- internal/controller/restapi/router.go
+++ internal/controller/restapi/router.go
@@ -1,17 +1,24 @@
 package restapi
 
 import (
+	"github.com/andreyxaxa/Image-Processor/config"
 	v1 "github.com/andreyxaxa/Image-Processor/internal/controller/restapi/v1"
 	"github.com/andreyxaxa/Image-Processor/internal/usecase"
 	"github.com/andreyxaxa/Image-Processor/pkg/logger"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 )
 
 // @title Image processor
 // @version 1.0.0
 // @host localhost:8080
 // @BasePath /v1
-func NewRouter(app *fiber.App, img usecase.ImageUseCase, l logger.Interface) {
+func NewRouter(app *fiber.App, cfg *config.Config, img usecase.ImageUseCase, l logger.Interface) {
+	// Swagger
+	if cfg.Swagger.Enabled {
+		app.Get("/swagger/*", swagger.HandlerDefault)
+	}
+
 	// Routers
 	apiV1Group := app.Group("/v1")
 	{
