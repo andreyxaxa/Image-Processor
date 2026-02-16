b805a785f580abdade2f26870f7829bd3dce4936
Petrov Andrey <4ndrew1337@mail.ru>
2026-02-16 03:09:01 +0300

    feat(db): define ImageMetadataRepo interface

--- internal/repo/contracts.go
+++ internal/repo/contracts.go
@@ -3,6 +3,9 @@ package repo
 import (
 	"context"
 	"io"
+
+	"github.com/andreyxaxa/Image-Processor/internal/entity"
+	"github.com/google/uuid"
 )
 
 type (
@@ -12,5 +15,13 @@ type (
 		Download(ctx context.Context, key string) (io.ReadCloser, error)
 		DownloadBytes(ctx context.Context, key string) ([]byte, error)
 		Delete(ctx context.Context, key string) error
+	}
+
+	ImageMetadataRepo interface {
+		Create(ctx context.Context, image *entity.Image) error
+		GetByID(ctx context.Context, id uuid.UUID) (*entity.Image, error)
+		GetProcessedKeyByID(ctx context.Context, id uuid.UUID) (string, string, error)
+		Update(ctx context.Context, image *entity.Image) error
+		Delete(ctx context.Context, id uuid.UUID) error
 	}
 )
